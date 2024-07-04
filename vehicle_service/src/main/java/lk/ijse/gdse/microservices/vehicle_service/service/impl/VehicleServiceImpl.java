0ff3ec0cbc9cd1aebf99162b126be52fe1cb6525
ravindudinushan <ravindudinushan9966@gmail.com>
2024-07-04 12:13:33 +0530

    completed

--- vehicle_service/src/main/java/lk/ijse/gdse/microservices/vehicle_service/service/impl/VehicleServiceImpl.java
+++ vehicle_service/src/main/java/lk/ijse/gdse/microservices/vehicle_service/service/impl/VehicleServiceImpl.java
@@ -1,6 +1,5 @@
 package lk.ijse.gdse.microservices.vehicle_service.service.impl;
 
-import jakarta.ws.rs.NotFoundException;
 import lk.ijse.gdse.microservices.vehicle_service.dto.VehicleDTO;
 import lk.ijse.gdse.microservices.vehicle_service.entity.Vehicle;
 import lk.ijse.gdse.microservices.vehicle_service.repo.VehicleRepo;
@@ -29,6 +28,7 @@ public class VehicleServiceImpl implements VehicleService {
         if(vehicleRepo.existsById(vehicleDTO.getRegistration_number())){
             throw new RuntimeException("Customer Id "+vehicleDTO.getRegistration_number()+" All ready exist");
         }
+
         Vehicle vehicle = modelMapper.map(vehicleDTO,Vehicle.class);
         vehicleRepo.save(vehicle);
     }
@@ -38,6 +38,7 @@ public class VehicleServiceImpl implements VehicleService {
         if(!vehicleRepo.existsById(vehicleDTO.getRegistration_number())){
             throw new RuntimeException("Customer Id "+vehicleDTO.getRegistration_number()+" does not exist");
         }
+
         vehicleRepo.save(modelMapper.map(vehicleDTO,Vehicle.class));
     }
 }
