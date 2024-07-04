c766bf7a6762de37c6bfe7cdd7db9bf8a412e62f
ravindudinushan <ravindudinushan9966@gmail.com>
2024-07-04 12:12:12 +0530

    completed

--- vehicle_service/src/main/java/lk/ijse/gdse/microservices/vehicle_service/dto/VehicleDTO.java
+++ vehicle_service/src/main/java/lk/ijse/gdse/microservices/vehicle_service/dto/VehicleDTO.java
@@ -10,41 +10,31 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class VehicleDTO {
-
     @NotNull
     @NotBlank(message = "registration number can be not null")
     private String registration_number;
-
     @NotNull
     @NotBlank(message = "vehicle chassis number can be not null")
     private String vehicle_chassis_number;
-
     @NotNull
     @NotBlank(message = "vehicle type can be not null")
     private String vehicle_type;
-
     @NotNull
     @NotBlank(message = "vehicle model can be not null")
     private String vehicle_model;
-
     @NotNull
     @NotBlank(message = "vehicle year can be not null")
     private String vehicle_year;
-
     @NotNull
     @NotBlank(message = "vehicle color can be not null")
     private String vehicle_color;
-
     @NotNull
     @NotBlank(message = "vehicle engine can be not null")
     private String vehicle_engine;
-
     @NotNull
     @NotBlank(message = "vehicle fuel type can be not null")
     private String vehicle_fuel_type;
-
     @NotNull
     @NotBlank(message = "user NIC can be not null")
     private String userNIC;
-
 }
