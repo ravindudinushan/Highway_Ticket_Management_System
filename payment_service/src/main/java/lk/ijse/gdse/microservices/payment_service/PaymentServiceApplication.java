aaa54adbdc7dcfafd981665d432b6fd61a0a95e2
ravindudinushan <ravindudinushan9966@gmail.com>
2024-07-04 11:54:05 +0530

    completed

--- payment_service/src/main/java/lk/ijse/gdse/microservices/payment_service/PaymentServiceApplication.java
+++ payment_service/src/main/java/lk/ijse/gdse/microservices/payment_service/PaymentServiceApplication.java
@@ -12,7 +12,6 @@ public class PaymentServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentServiceApplication.class, args);
     }
-
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
