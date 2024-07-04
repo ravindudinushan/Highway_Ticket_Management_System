e830046ff47848cc9c7ec713f81d86292d37ea2a
ravindudinushan <ravindudinushan9966@gmail.com>
2024-07-04 12:03:02 +0530

    completed

--- ticket_service/src/main/java/lk/ijse/gdse/microservices/ticket_service/repo/TicketRepo.java
+++ ticket_service/src/main/java/lk/ijse/gdse/microservices/ticket_service/repo/TicketRepo.java
@@ -4,6 +4,5 @@ import lk.ijse.gdse.microservices.ticket_service.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TicketRepo extends JpaRepository<Ticket, Integer> {
-    //Ticket findByT(int ticket_no);
 
 }
