44518ad451c8ffa7868e781727ed29d30ff969aa
ravindudinushan <ravindudinushan9966@gmail.com>
2024-07-04 12:09:02 +0530

    completed

--- user_service/src/main/java/lk/ijse/gdse/microservices/user_service/service/UserService.java
+++ user_service/src/main/java/lk/ijse/gdse/microservices/user_service/service/UserService.java
@@ -5,7 +5,6 @@ import lk.ijse.gdse.microservices.user_service.dto.UserDTO;
 import java.util.List;
 
 public interface UserService {
-
     List<UserDTO> getAllUser();
     void registerUser(UserDTO userDTO);
     void updateUser(UserDTO userDTO);
